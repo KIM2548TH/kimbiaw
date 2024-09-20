693d596b304806f6b4f95ea75f18aacba7de9722
KIM2548TH <jitkon1979@gmail.com>
2024-09-20 20:36:23 +0700

    add

--- funlab/web/views/course.py
+++ funlab/web/views/course.py
@@ -8,8 +8,8 @@ from flask import (
 from flasklogin import current_user, login_required
 from funlab import models
 
-model = Blueprint("cours",__name, url_prefix="/course")
-@models.route("/", methods=["GET, 'POST"])
+module = Blueprint("cours",__name, url_prefix="/course")
+@module.route("/", methods=["GET, 'POST"])
 def index():
     course = models.Course.object()
     return render_template()
